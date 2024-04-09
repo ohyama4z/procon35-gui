2e53baf98222c72635f4fc2d5f169869972be7ef
ohyama4z <ohyama4z000@gmail.com>
2024-04-09 12:01:48 +0900

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { css } from "../styled-system/css";
+
+export default function App() {
+  return (
+    <div className={css({
+      fontSize: "6xl",
+      fontWeight: "bold",
+    })}>
+      Hello World
+    </div>
+  )
+}
